adfa1f6f02e7d2b7b48cdc1890747b718e2400c4
thaylon56 <thaylomarthur760@gmail.com>
2026-06-06 00:51:28 -0400

    Add files via upload

--- org sit/js/config.js
+++ org sit/js/config.js
@@ -4,6 +4,6 @@
  * Em desenvolvimento local, preencha com suas credenciais
  */
 window.SUPABASE_CONFIG = {
-  url: 'https://seu-projeto.supabase.co',
-  anonKey: 'sua-chave-anon-aqui'
+  url: 'https://tadnupgdedtlsgaazwaz.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhZG51cGdkZWR0bHNnYWF6d2F6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA3MDg5OTMsImV4cCI6MjA5NjI4NDk5M30.FfdnF4_ZU-CGm86eVXihnVlkHtYyBLW75-vRU9lj_MUv '
 };
